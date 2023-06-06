bd67fcbf3798c9ab0d7bf90a065296d6cacf6866
Pappu Mahato <pappumahato561@gmail.com>
2023-06-06 18:23:25 +0530

    Setup Completed For Firebase

---
+++ learning-angular-firebase/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+      apiKey: "AIzaSyCLPWTVFJjVsJvCxQ8eDwCewSXz6XMN0JE",
+  authDomain: "learning-angular-firebas-e5066.firebaseapp.com",
+  projectId: "learning-angular-firebas-e5066",
+  storageBucket: "learning-angular-firebas-e5066.appspot.com",
+  messagingSenderId: "403584393318",
+  appId: "1:403584393318:web:9c997719f641c6b48c4da7"
+},
+  };
